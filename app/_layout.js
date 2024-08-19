0fa046e79985aee5ca2c31ee7c7907020fba4b05
terukimateus <terukimateus@outlook.com>
2024-08-19 18:39:59 -0300

    changes

--- app/_layout.js
+++ app/_layout.js
@@ -1,45 +1,19 @@
-import React, { useEffect } from "react";
-import { router } from 'expo-router';
+import React from "react";
+import { Slot, router } from 'expo-router'
 import { Stack } from 'expo-router/stack';
-import { UserProvider, useUser } from './(tabs)/context/user.context';
-import * as SplashScreen from 'expo-splash-screen';
-
-SplashScreen.preventAutoHideAsync();
-
-function RootLayoutInner() {
-  const { loading, user } = useUser();
-
-  useEffect(() => {
-    if (!loading) {
-      SplashScreen.hideAsync();
-    }
-  }, [loading]);
-
-  useEffect(() => {
-    if (!loading) {  // Certifique-se de que a navegação condicional também dependa de `loading`
-      if (user) {
-        router.push('/home');
-      } else {
-        router.push('/login'); // ou qualquer outra tela de login que você tenha
-      }
-    }
-  }, [loading, user]);
-
-  if (loading) {
-    return null; // ou você pode exibir uma tela de carregamento
-  }
-
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
+import { UserProvider } from './(tabs)/context/user.context';
 
 export default function RootLayout() {
+  
+  React.useEffect(() => {
+    router.push('/home');
+  }, []);
+
   return (
     <UserProvider>
-      <RootLayoutInner />
+      <Stack screenOptions={{headerShown: false}}>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
     </UserProvider>
   );
 }
